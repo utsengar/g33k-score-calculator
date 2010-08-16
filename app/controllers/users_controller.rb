572582fa62e04471246d69c85e983a88d6409e96
Utkarsh Sengar <utkarsh2012@gmail.com>
2010-08-15 17:04:59 -0700

    added validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,21 @@ class UsersController < ApplicationController
   def register_user
     if(!params[:user].nil?)
      #user = User.create(params[:user]) #user saved in database
-     geek_score = service_call(params[:user][:stackoverflow_id], params[:user][:github_id])
-     @final_value = calculate_score(geek_score)
+     so_url = params[:user][:stackoverflow_id]
+     github_id = params[:user][:github_id]
+     
+     if(!so_url.empty? && !github_id.empty?)
+       #Make the service call and get the score
+       geek_score = service_call(so_url, github_id)
+       
+       #return values
+       @final_value = geek_score[0]
+       @so_reputation = geek_score[1]
+       @gh_followers = geek_score[2]
+       @gh_repos = geek_score[3]
+     else
+       
+     end
     end
   end
   
@@ -33,7 +46,13 @@ class UsersController < ApplicationController
       reputation = user_Hash['reputation']
       
       #Geekness calculation benchmarks
-      if(reputation > 100)
+      if(reputation >= 1000)
+        geekness = geekness + 50
+      elsif(reputation >= 500)
+        geekness = geekness + 25
+      elsif(reputation >= 100)
+        geekness = geekness + 10
+      elsif(reputation < 100)
         geekness = geekness + 5
       end
     end
@@ -54,20 +73,28 @@ class UsersController < ApplicationController
       score = user_Hash["score"] #5.03
       
       #Geekness calculation benchmarks
-      if(followers > 10)
-        geekness = geekness + 2.5
+      if(followers >= 10)
+        geekness = geekness + 25
+      elsif(followers >= 5)
+        geekness = geekness + 12.5
+      elsif(followers >=2 )
+        geekness = geekness + 5
+      elsif(followers >=1 )
+        geekness = geekness + 1
       end
+      
       if(repos > 10)
-        geekness = geekness + 2.5
+        geekness = geekness + 25
+      elsif(repos >=5 )
+        geekness = geekness + 12.5
+      elsif(repos >=2 )
+        geekness = geekness + 10
+      elsif(repos >=1 )
+        geekness = geekness + 5
       end
    end
    
-   return geekness
-  end
-  
-  def calculate_score(geek_score)
-    
-    
-    return geek_score
+   return_array = [geekness, reputation, followers, repos]
+   return return_array
   end
 end
